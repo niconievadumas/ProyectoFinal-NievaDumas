8c8f1ee9074ea3368960603f13f6db1667eca7e2
Nico Nieva Dumas <niconievadumasgmail.com>
2022-07-25 21:08:54 -0300

    cargados los esqueletos html

--- EntregaFinalNievaDumas/settings.py
+++ EntregaFinalNievaDumas/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inicio',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'EntregaFinalNievaDumas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'/Users/niconievadumas/Documents/Programacion/CODER/ENTREGAFINAL_NIEVA_DUMAS/templates/', r"C:\Users\Usuario\Desktop\Programacion\CODER\Proyectos\PROYECTO 1\Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
