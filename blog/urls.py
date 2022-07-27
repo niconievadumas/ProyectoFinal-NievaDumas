6c6b6071df5fdf3add2f56aad9a812a032e89e31
Nico Nieva Dumas <niconievadumas@gmail.com>
2022-07-27 13:31:53 -0300

    casi terminado. falta la mensajeria

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import ListadoPosteos, crear_posteo, post
+from .views import EliminarPosteo, ListadoPosteos, MostrarPosteo, crear_posteo, editar_posteo
 
 urlpatterns = [
     path('listado_posteos/', ListadoPosteos.as_view(), name= "listado_posteos"),
-    path('post/', post, name= "post"),
+    path('posteo/<int:pk>/', MostrarPosteo.as_view(), name= "posteo"),
     path('crear-posteo/', crear_posteo , name= "crear_posteo"),
-    # path('editar-gato/<int:pk>/', views.EditarGato.as_view(), name= "editar_gato"),
-    # path('eliminar-gato/<int:pk>/', views.EliminarGato.as_view(), name= "eliminar_gato"),
-    # path('mostrar-gato/<int:pk>/', views.MostrarGato.as_view(), name= "mostrar_gato")
+    path('editar-posteo/<int:id>/', editar_posteo, name= "editar_posteo"),
+    path('eliminar-posteo/<int:pk>/', EliminarPosteo.as_view(), name= "eliminar_posteo"),
 ]
    
